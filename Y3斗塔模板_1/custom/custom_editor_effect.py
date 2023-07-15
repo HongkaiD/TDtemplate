420c2a3cccec9a0c7dae7cd5340159455cdca5d3
HongkaiD <jamesding22d@gmail.com>
2023-07-15 21:25:46 +0800

    完成羁绊、升级查重

---
+++ Y3斗塔模板_1/custom/custom_editor_effect.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {134222392: {'model_id': 134244480, 'preview_icon': '', 'name': u'\u84dd\u8272\u7f51\u683c\u5149\u6548', 'describe': 134222392, 'key': 134222392, 'res_identify': '', 'scale': 1.0, 'uid': '134222392'}}
---
+++ Y3斗塔模板_1/custom/custom_model.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {134244480: {'name': u'\u84dd\u8272\u7f51\u683c\u5149\u6548', 'model_id': 134244480, 'effect_path': 'Fx_Effect/OtherType/Prop/Prop_zuozhe_qipai_a0003_copy', 'meshes': (), 'custom': True}}
